d74377ce37b928bfc241c56c7b2dd6c3a3a2fac9
KB731 <kevinbette.formapro@gmail.com>
2022-03-03 14:20:58 +0100

    Calculatrice_V1

---
+++ index.php
@@ -0,0 +1,8 @@
+<form method="post" action="form.php">
+Sélectionnez un nombre: <input type="text" name="nbr1" /><br />
+Sélectionnez le signe (+, -, /, *) : <input type="text" name="signe" /><br />
+Sélectionnez le deuxième nombre: <input type="text" name="nbr2" /><br />
+<br />
+<input type="submit" valu="Faire le calcul" />
+</form>
+
